b12d147a85fe63161d8b9c38e8d696a7edf5953e
Jay Paygude <jaypaygude2@gmail.com>
2025-08-22 11:38:10 +0530

    Create print_pointer.cpp

---
+++ print_pointer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  char str[100];
+  cout<<"Enter a single word: ";
+  cin>>str;
+
+  cout<<"Original string: "<<str<<endl;
+
+  char *ptr=str;
+  cout<<"String printed using pointer: ";
+  while(*ptr!='\0'){
+    cout<<*ptr;
+    ptr++;
+  }
+  return 0;
+}
