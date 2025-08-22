9cf92f91562403fe9f1abde51e417af07b6c3924
Jay Paygude <jaypaygude2@gmail.com>
2025-08-22 11:35:00 +0530

    Create basic_pointer_arithmetic.cpp

---
+++ basic_pointer_arithmetic.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int a=45,*aptr; 
+ bool b=true,*bptr;
+ float c=a,*cptr;
+ double d=10.66466464,*dptr;
+
+ aptr=&a; bptr=&b; cptr=&c; dptr=&d;
+
+ cout<<"Integer address before increment:"<<aptr<<endl;
+ aptr++;
+ cout<<"Integer address after increment:"<<aptr<<endl;
+
+ cout<<"Boolean address before increment:"<<bptr<<endl;
+ bptr++;
+ cout<<"Boolean address after increment:"<<bptr<<endl;
+
+ cout<<"Float address before increment:"<<cptr<<endl;
+ cptr++;
+ cout<<"Float address after increment:"<<cptr<<endl;
+
+ cout<<"Double address before increment:"<<dptr<<endl;
+ dptr++;
+ cout<<"Double address after increment:"<<dptr<<endl;
+
+ return 0;
+}
