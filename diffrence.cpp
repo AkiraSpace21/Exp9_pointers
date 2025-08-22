8c0aacad1b8274e8196e309abd8ab75a808d73f3
Jay Paygude <jaypaygude2@gmail.com>
2025-08-22 11:37:12 +0530

    Create diffrence.cpp

---
+++ diffrence.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int arr[]={10,30,50,70,90};
+  int *p1=&arr[0], *p2=&arr[1], *p3=&arr[2], *p4=&arr[3], *p5=&arr[4];
+
+  int diff=*p5-*p3;
+  cout<<"Difference of p5 & p3: "<<diff<<endl;
+
+  int add=*p1+*p2+*p3+*p4+*p5;
+  cout<<"Addition of all pointers: "<<add<<endl;
+
+  int prod=(*p2)*(*p4);
+  cout<<"Product of p2 & p4: "<<prod<<endl;
+  return 0;
+}
